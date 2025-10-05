7354249c2a129d6f4ae775e285b9c05a41406ac3
mem-5514-tahara <tahara.yo.5514@members.co.jp>
2025-10-06 01:55:45 +0900

    docs: Add usage example for qr_image_exporter

--- example/qr_image_exporter_example.dart
+++ example/qr_image_exporter_example.dart
@@ -1,6 +1,25 @@
+import 'dart:io';
+import 'package:qr/qr.dart';
 import 'package:qr_image_exporter/qr_image_exporter.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // Create QR code data
+  final qrCode = QrCode.fromData(
+    data: 'Hello, World!',
+    errorCorrectLevel: QrErrorCorrectLevel.M,
+  );
+  final qrImage = QrImage(qrCode);
+
+  // Export PNG data
+  final pngBytes = qrImage.toPngBytes(
+    moduleSize: 8,
+    margin: 40,
+    darkColor: 0xFF0066AA, // Blue
+  );
+
+  if (pngBytes != null) {
+    // Save as file (example)
+    File('my_qr_code.png').writeAsBytesSync(pngBytes);
+    print('QR code saved as my_qr_code.png.');
+  }
 }
