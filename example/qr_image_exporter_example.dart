074385fe764842cf222232111957a9926d1cc2b1
mem-5514-tahara <tahara.yo.5514@members.co.jp>
2025-10-06 01:32:53 +0900

    initial commit

---
+++ example/qr_image_exporter_example.dart
@@ -0,0 +1,6 @@
+import 'package:qr_image_exporter/qr_image_exporter.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/qr_image_exporter_test.dart
@@ -0,0 +1,16 @@
+import 'package:qr_image_exporter/qr_image_exporter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
